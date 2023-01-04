064f1193ce18129aecb8d78467143ac24091fcd7
Caio Figueirôa <cfigueiroa@gmail.com>
2023-01-04 13:40:44 -0300

    feat: add POST /sign-up

--- src/app.js
+++ src/app.js
@@ -5,17 +5,38 @@ class Server {
     constructor() {
         this.app = express();
         this.app.use(cors());
+        this.app.use(express.json());
 
-        this.app.post('/sign-up', (_req, res) => {
-            res.send('OK');
+        this.users = [];
+
+        this.spaceOnly = (...args) => !args.every(arg => arg.trim());
+
+        this.app.post('/sign-up', (req, res) => {
+            if (!req.body?.username || !req.body?.avatar) {
+                return res.status(400).send('Todos os campos são obrigatórios!');
+            }
+
+            const { username, avatar } = req.body;
+
+            if (this.spaceOnly(username, avatar)) {
+                return res.status(400).send('Todos os campos são obrigatórios!');
+            }
+
+            if (this.users.find(user => user.username === username)) {
+                return res.status(400).send('O usuário já existe!');
+            }
+
+            const newUser = { username, avatar };
+            this.users.push(newUser);
+            return res.send('OK');
         });
 
         this.app.post('/tweets', (_req, res) => {
-            res.send('OK');
+            return res.send('OK');
         });
 
         this.app.get('/tweets', (_req, res) => {
-            res.send('OK');
+            return res.send('OK');
         });
     }
 
